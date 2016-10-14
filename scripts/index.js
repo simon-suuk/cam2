140940177b74c3879f3278b0d0c77904a8208ea4
simon <simon.suuk@outlook.com>
2016-10-14 12:13:58 +0000

    camera test initial

---
+++ scripts/index.js
@@ -0,0 +1,37 @@
+﻿(function() {
+
+	document.addEventListener('deviceready', onDeviceReady.bind(this), false);
+	var pictureSource;
+	var destinationType;
+	function onDeviceReady() {
+		pictureSource = navigator.camera.PictureSourceType;
+		destinationType = navigator.camera.DestinationType;
+
+		document.getElementById("capturePhoto").onclick = function() {
+			navigator.camera.getPicture(onPhotoDataSuccess, onFail, {
+				quality : 50,
+
+				destinationType : destinationType.DATA_URL
+			});
+		}
+		  
+		  
+	
+	};
+	function onPhotoDataSuccess(imageData) {
+
+		var smallImage = document.getElementById('smallImage');
+
+		smallImage.style.display = 'block';
+
+		smallImage.src = "data:image/jpeg;base64," + imageData;
+
+	}
+
+	function onFail(message) {
+
+		alert('Failed because: ' + message);
+
+	}
+
+})();
